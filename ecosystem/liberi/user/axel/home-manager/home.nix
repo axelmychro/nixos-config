00b0c9194c42c564be603b0f92971cf5c85add99
Axel <axelmychro@gmail.com>
2026-03-12 18:28:52 +0700

    ref: update directory

--- ecosystem/liberi/user/axel/home-manager/home.nix
+++ ecosystem/liberi/user/axel/home-manager/home.nix
@@ -6,7 +6,7 @@
   home-manager = {
     extraSpecialArgs = { inherit noctalia; };
     users.axel = {
-      xdg.configFile."niri/config.kdl".source = ../config/config.kdl;
+      xdg.configFile."niri/config.kdl".source = ../config/niri/config.kdl;
       imports = [
         ./programs/index.nix
         ./noctalia-shell/index.nix
