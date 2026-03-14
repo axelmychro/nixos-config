db460249778a73a88da24bde85ce2e839432c632
Axel <axelmychro@gmail.com>
2026-03-14 19:53:10 +0700

    ref: merge functions into flags

--- ecosystem/skadi/user/axel/home-manager/shell/fish/functions.nix
+++ ecosystem/skadi/user/axel/home-manager/shell/fish/functions.nix
@@ -2,24 +2,33 @@ _: {
   programs.fish.functions = {
     generate = {
       body = ''
+        argparse r/reboot u/update w/wipe -- $argv
+        or return
+
+        if set -q _flag_update
+          nix flake update
+        end
+        if set -q _flag_wipe
+            nix profile wipe-history
+            nix store gc
+            nix store optimise
+        end
+
         fd -e nix -x nixfmt {}
         fd -e kdl -x kdlfmt format {}
         git add .
-        sudo nixos-rebuild switch --flake .#skadi
-      '';
-    };
-    generate-bleed = {
-      body = ''
-        nix flake update
-        generate
-      '';
-    };
-    generate-wipe = {
-      body = ''
-        nix profile wipe-history
-        nix store gc
-        nix store optimise
-        generate
+
+        if sudo nixos-rebuild switch --flake .#skadi
+          if set -q _flag_reboot
+            echo "Rebooting in 5 seconds... (Ctrl+C to cancel)"
+            sleep 5
+            systemctl reboot
+          end
+          return 0
+        else
+          echo "generate failed."
+          return 1
+        end
       '';
     };
   };
