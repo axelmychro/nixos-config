ba1737ff68d0deddbdb59a786edb38348186738f
Axel <axelmychro@gmail.com>
2026-03-15 10:03:06 +0700

    ref: adjust cursor theme

--- user/axel/home-manager/home.nix
+++ user/axel/home-manager/home.nix
@@ -1,17 +1,29 @@
-{ nixcord, ... }:
+{ pkgs, nixcord, ... }:
 {
   home-manager = {
     users.axel = {
       home = {
         homeDirectory = "/home/axel";
         stateVersion = "24.11";
+        pointerCursor = {
+          enable = true;
+          package = pkgs.nordzy-cursor-theme;
+          name = "Nordzy-cursors-white";
+          size = 48;
+
+          x11 = {
+            enable = true;
+            defaultCursor = "left_ptr";
+          };
+          gtk.enable = true;
+        };
       };
       xdg.enable = true;
       imports = [
         nixcord.homeModules.nixcord
         ./default-apps/index.nix
-        ./shell/index.nix
         ./programs/index.nix
+        ./shell/index.nix
       ];
     };
     useGlobalPkgs = true;
