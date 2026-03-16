d9b59948e2921b0d77614cd663c20cde7b013ee6
Axel <axelmychro@gmail.com>
2026-03-16 12:16:18 +0700

    init: zen browser preferences

--- user/axel/home-manager/programs/zen-browser/index.nix
+++ user/axel/home-manager/programs/zen-browser/index.nix
@@ -3,17 +3,18 @@
   ...
 }:
 {
+  programs.zen-browser = {
+    enable = true;
+    nativeMessagingHosts = [ pkgs.firefoxpwa ];
+    profiles.default.isDefault = true;
+  };
   imports = [
     ./bookmarks.nix
     ./extensions.nix
     ./pins.nix
     ./policies.nix
     ./search.nix
+    ./settings.nix
     ./spaces.nix
   ];
-  programs.zen-browser = {
-    enable = true;
-    nativeMessagingHosts = [ pkgs.firefoxpwa ];
-    profiles.default.isDefault = true;
-  };
 }
---
+++ user/axel/home-manager/programs/zen-browser/settings.nix
@@ -0,0 +1,15 @@
+{
+  programs.zen-browser.profiles.default.settings = {
+    "browser.display.use_document_fonts" = 0;
+
+    "font.name.serif.x-western" = "UbuntuSans Nerd Font";
+    "font.name.sans-serif.x-western" = "ZedMono Nerd Font";
+    "font.name.monospace.x-western" = "FiraCode Nerd Font";
+
+    "font.size.variable.x-western" = 16;
+    "font.size.monospace.x-western" = 16;
+
+    "browser.tabs.warnOnClose" = true;
+    "browser.download.panel.shown " = false;
+  };
+}
