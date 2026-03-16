c4652642e38d0af96434212435357d50c942770d
Axel <axelmychro@gmail.com>
2026-03-16 11:59:10 +0700

    chore: remove control center, rearrange widgets

--- ecosystem/skadi/user/axel/home-manager/noctalia-shell/settings/bar/widgets.nix
+++ ecosystem/skadi/user/axel/home-manager/noctalia-shell/settings/bar/widgets.nix
@@ -1,13 +1,12 @@
-{ config, skadiAssets, ... }:
+{ skadiAssets, ... }:
 {
   home.file.".icon".source = "${skadiAssets}/icon.png";
   programs.noctalia-shell.settings.bar.widgets = {
     left = [
       {
-        id = "ControlCenter";
-        enableColorization = true;
-        useDistroLogo = false;
-        customIconPath = "${config.home.homeDirectory}/.icon";
+        id = "Workspace";
+        hideUnoccupied = true;
+        labelMode = "none";
       }
       {
         id = "Network";
@@ -15,38 +14,28 @@
       {
         id = "Bluetooth";
       }
-      {
-        id = "PowerProfile";
-      }
     ];
     center = [
       {
-        id = "Workspace";
-        hideUnoccupied = false;
-        labelMode = "none";
+        id = "Clock";
+        formatHorizontal = "hh:mm A";
+        formatVertical = "hh mm A";
+        useMonospacedFont = true;
+        usePrimaryColor = true;
       }
     ];
     right = [
       {
-        formatHorizontal = "dd MMM, hh:mm a";
-        formatVertical = "dd MMM hh mm";
-        id = "Clock";
-        useMonospacedFont = true;
-        usePrimaryColor = true;
-      }
-      {
-        id = "Brightness";
-      }
-      {
-        id = "Volume";
-      }
-      {
         id = "Battery";
+        displayMode = "graphic";
         showPercentageText = true;
+        showPowerProfiles = true;
+        showNoctaliaPerformance = true;
         warningThreshold = 50;
       }
       {
         id = "Tray";
+        chevronColorKey = "mSecondary";
       }
       {
         id = "NotificationHistory";
