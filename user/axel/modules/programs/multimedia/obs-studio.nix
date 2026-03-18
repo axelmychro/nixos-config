608d2a45116f5b5401f59da7c3bcc78f1bf39aec
Axel <axelmychro@gmail.com>
2026-03-18 17:35:23 +0700

    chore: obs plugins

--- user/axel/modules/programs/multimedia/obs-studio.nix
+++ user/axel/modules/programs/multimedia/obs-studio.nix
@@ -1,1 +1,18 @@
-_: { programs.obs-studio.enable = true; }
+{ pkgs, ... }:
+{
+  programs.obs-studio = {
+    enable = true;
+    package = (
+      pkgs.obs-studio.override {
+        cudaSupport = true;
+      }
+    );
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      # obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+}
