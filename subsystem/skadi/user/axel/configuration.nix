511b0dba2a9b7da2230bc9a1af1cc539066f6802
Axel <axelmychro@gmail.com>
2026-03-27 11:00:40 +0700

    fix: double declaration

--- subsystem/skadi/user/axel/configuration.nix
+++ subsystem/skadi/user/axel/configuration.nix
@@ -1,7 +1,5 @@
 { pkgs, ... }:
 {
-  programs.fish.enable = true;
-  users.users.axel.shell = pkgs.fish;
   imports = [
     ./home-manager/home.nix
   ];
--- user/axel/configuration.nix
+++ user/axel/configuration.nix
@@ -7,6 +7,7 @@
       extraGroups = [ "wheel" ];
     };
   };
+  programs.fish.enable = true;
   imports = [
     ./modules/programs/index.nix
     ./modules/services/index.nix
