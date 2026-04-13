db4a1f0b5de1fe82b3314e4853e14ae5e2a7ff65
Axel <axelmychro@gmail.com>
2026-04-13 17:14:40 +0700

    chore: delete unused script

--- user/axel/home-manager/shell/fish/abbrs/nix.nix
+++ user/axel/home-manager/shell/fish/abbrs/nix.nix
@@ -1,11 +1,11 @@
 _: {
   programs.fish.shellAbbrs = {
     nrs = "sudo nixos-rebuild switch";
-    nrsf = "sudo nixos-rebuild switch --flake .#skadi";
+    nrsf = "sudo nixos-rebuild switch --flake .#";
     nrt = "sudo nixos-rebuild test";
-    nrtf = "sudo nixos-rebuild test --flake .#skadi";
+    nrtf = "sudo nixos-rebuild test --flake .#";
     nrd = "sudo nixos-rebuild dry-run";
-    nrdf = "sudo nixos-rebuild dry-run --flake .#skadi";
+    nrdf = "sudo nixos-rebuild dry-run --flake .#";
     nu = "nix flake update";
 
     nso = "nix store optimise";
