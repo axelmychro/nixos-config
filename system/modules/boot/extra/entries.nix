3a2d081321dcd6d5e2b7b1386b0bf6210bcd034a
Axel <axelmychro@gmail.com>
2026-05-05 12:34:56 +0700

    grub: update entry version

--- system/modules/boot/extra/entries.nix
+++ system/modules/boot/extra/entries.nix
@@ -3,9 +3,9 @@ let
   bootPartition = "800C-0267";
   gentooPartition = "/dev/nvme0n1p3";
   gentooKernelDir = "kernels-gentoo";
-  gentooKernelLabel = "7.0.3-gentoo-dist";
+  gentooKernelLabel = "7.0.5-gentoo-dist";
   zenKernelDir = "kernels-zen";
-  zenKernelLabel = "7.0.3-zen1";
+  zenKernelLabel = "7.0.5-zen1";
 in
 {
   boot.loader.grub.extraEntries = ''
@@ -14,15 +14,15 @@ in
         insmod part_gpt
         insmod fat
         search --no-floppy --fs-uuid --set=root ${bootPartition}
-        linux /${gentooKernelDir}/vmlinuz-${gentooKernelLabel} root=${gentooPartition} rw quiet loglevel=3 nowatchdog fsck.mode=force fsck.repair=yes 
-        initrd /${gentooKernelDir}/initramfs-${gentooKernelLabel}.img
+        linux /${gentooKernelDir}/vmlinuz-${gentooKernelLabel} root=${gentooPartition} rw quiet loglevel=3 nowatchdog fsck.mode=force fsck.repair=yes
+        # initrd /${gentooKernelDir}/initramfs-${gentooKernelLabel}.img
       }
       menuentry 'zen-sources' --class gentoo --class gnu-linux --class gnu --class os {
         insmod part_gpt
         insmod fat
         search --no-floppy --fs-uuid --set=root ${bootPartition}
         linux /${zenKernelDir}/vmlinuz-${zenKernelLabel} root=${gentooPartition} rw quiet
-        initrd /${zenKernelDir}/initramfs-${zenKernelLabel}.img
+        # initrd /${zenKernelDir}/initramfs-${zenKernelLabel}.img
       }
     }
   '';
