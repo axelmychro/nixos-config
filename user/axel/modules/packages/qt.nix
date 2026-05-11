4ab234474301cbcbe88ce5bfa448c5882a9a8072
Axel <axelmychro@gmail.com>
2026-05-11 12:34:56 +0700

    ref: lay out qt packages

--- user/axel/modules/packages/qt.nix
+++ user/axel/modules/packages/qt.nix
@@ -1,23 +1,19 @@
 { pkgs, ... }:
-let
-  # pkgs.qt6.env already includes pkgs.qt6.qtbase
-  # And using `with` to prevent a lot of typing.
-  qtEnv =
-    with pkgs.qt6;
-    env "qt-custom-${qtbase.version}" [
-      qtbase
-      qttools
-      qtpositioning
-      qtdeclarative
-    ];
-in
 {
-  environment.systemPackages = [
-    qtEnv
-    # pkgs.qt6.qtdeclarative depends on pkgs.libglvnd
-    pkgs.libglvnd
-    pkgs.qtcreator
-    pkgs.gnumake
-    pkgs.cmake
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      qt6.qtbase
+      qt6.qt5compat
+      qt6.qttools
+      qt6.qtpositioning
+      qt6.qtdeclarative
+      # qt6.qtdeclarative depends on libglvnd
+      libglvnd
+      qtcreator
+      gcc
+      gnumake
+      cmake
+      gdb
+    ];
+  };
 }
