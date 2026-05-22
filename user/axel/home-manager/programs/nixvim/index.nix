3aa47ad3154ab0bc45e394109c3f66c282103b24
Axel <axelmychro@gmail.com>
2026-05-22 12:34:56 +0700

    Nixvim(plugins): Conform-nvim format on save

--- user/axel/home-manager/programs/nixvim/index.nix
+++ user/axel/home-manager/programs/nixvim/index.nix
@@ -4,16 +4,29 @@ _: {
 
     colorschemes.catppuccin = {
       enable = true;
-      autoLoad = true;
       settings.flavour = "macchiato";
     };
+    colorscheme = "catppuccin";
 
     plugins = {
       lsp = {
         enable = true;
         inlayHints = true;
       };
-      conform-nvim.enable = true;
+      conform-nvim = {
+        enable = true;
+        settings.format_on_save = {
+          timeout_ms = 500;
+          lsp_fallback = true;
+        };
+      };
+      treesitter = {
+        enable = true;
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+        };
+      };
     };
   };
   imports = [ ./lang/index.nix ];
