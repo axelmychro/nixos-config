af399f946915670f2246543ce031bb70f7cf35c3
Axel <axelmychro@gmail.com>
2026-05-25 21:03:32 +0700

    niri: disable

--- environment/abyss/system/configuration.nix
+++ environment/abyss/system/configuration.nix
@@ -4,7 +4,7 @@
   ...
 }:
 {
-  programs.niri.enable = true;
+  programs.niri.enable = false;
   services.desktopManager.plasma6.enable = true;
   security.polkit.enable = true;
   services.gnome.gnome-keyring.enable = true;
