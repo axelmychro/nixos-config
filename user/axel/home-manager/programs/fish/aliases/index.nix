8b3728074911daf58e70dfb541403669bf79ec47
Axel <axelmychro@gmail.com>
2026-05-26 10:30:40 +0700

    Fish: Alias l for eza

--- user/axel/home-manager/programs/fish/aliases/index.nix
+++ user/axel/home-manager/programs/fish/aliases/index.nix
@@ -1,10 +1,8 @@
 _: {
   programs.fish.shellAliases = {
     x = "sync; clear; exec fish";
-    xx = "exit";
 
-    ls = "eza --group-directories-first --no-permissions --octal-permissions --icons=auto --git";
-    l = "ls -Ahl";
+    l = "eza -Ahl --group-directories-first --no-permissions --octal-permissions --icons=auto --git";
     c = "cd";
     ".." = "cd ..";
     "..." = "cd ../..";
