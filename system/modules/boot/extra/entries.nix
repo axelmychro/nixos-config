4b64c87756ec46f23c2d9e88ed24c80b2ef1e3e0
Axel <axelmychro@gmail.com>
2026-05-28 17:36:58 +0700

    Boot(entries): Move root off boot partition for extra entries

--- system/modules/boot/extra/entries.nix
+++ system/modules/boot/extra/entries.nix
@@ -1,27 +1,35 @@
 _:
 let
   bootPartition = "800C-0267";
-  gentooPartition = "/dev/nvme0n1p3";
-  gentooKernelDir = "kernels-gentoo";
-  zenKernelDir = "kernels-zen";
+  gentooPartition = "e04ea3ec-7f23-4caa-bee1-1b0ebc3247b0";
+  fedoraPartition = "05aa19b1-9cfe-4736-9ef4-ad1046b62ff0";
+  gentooKernelDir = "/boot/kernels-gentoo";
+  zenKernelDir = "/boot/kernels-zen";
 in
 {
   boot.loader.grub.extraEntries = ''
     submenu 'Gentoo' --class gentoo {
-      menuentry 'gentoo-kernel-bin' --class gentoo --class gnu-linux --class gnu --class os {
+      menuentry 'Gentoo Kernel' --class gentoo --class gnu-linux --class gnu --class os {
         insmod part_gpt
-        insmod fat
-        search --no-floppy --fs-uuid --set=root ${bootPartition}
-        linux /${gentooKernelDir}/vmlinuz root=${gentooPartition} rw quiet loglevel=3 nowatchdog fsck.mode=force fsck.repair=yes
-        initrd /${gentooKernelDir}/initramfs.img
+        insmod ext2
+        search --no-floppy --fs-uuid --set=root ${gentooPartition}
+        linux ${gentooKernelDir}/vmlinuz root=UUID=${gentooPartition} rw quiet loglevel=3 nowatchdog fsck.mode=force fsck.repair=yes
+        initrd ${gentooKernelDir}/initramfs.img
       }
-      menuentry 'zen-sources' --class gentoo --class gnu-linux --class gnu --class os {
+      menuentry 'Zen Kernel' --class gentoo --class gnu-linux --class gnu --class os {
         insmod part_gpt
-        insmod fat
-        search --no-floppy --fs-uuid --set=root ${bootPartition}
-        linux /${zenKernelDir}/vmlinuz root=${gentooPartition} rw quiet
-        initrd /${zenKernelDir}/initramfs.img
+        insmod ext2
+        search --no-floppy --fs-uuid --set=root ${gentooPartition}
+        linux ${zenKernelDir}/vmlinuz root=UUID=${gentooPartition} rw quiet
+        initrd ${zenKernelDir}/initramfs.img
       }
+    }
+    menuentry 'Fedora' --class fedora --class gnu-linux --class gnu --class os {
+      insmod part_gpt
+      insmod ext2
+      search --no-floppy --fs-uuid --set=root ${fedoraPartition}
+      linux /boot/vmlinuz root=UUID=${fedoraPartition} rw quiet loglevel=3 nowatchdog fsck.mode=force fsck.repair=yes
+      initrd /boot/initramfs.img
     }
   '';
 }
