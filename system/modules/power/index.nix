ec56e021d8789deb2f1a24b04ce54379b2ff0c45
Axel <axelmychro@gmail.com>
2026-06-05 08:42:25 +0700

    Modules(power): Split config

--- system/modules/power/index.nix
+++ system/modules/power/index.nix
@@ -1,43 +1,11 @@
 _: {
-  services = {
-    thermald = {
-      enable = true;
-      debug = true;
-    };
-    upower = {
-      enable = true;
-      percentageLow = 50;
-      percentageCritical = 20;
-      ignoreLid = true;
-    };
+  imports = [
+    ./thermald.nix
+    ./upower.nix
 
-    # major options below all conflict with each other
-    power-profiles-daemon.enable = true;
-
-    tlp = {
-      enable = false;
-      settings = {
-        START_CHARGE_THRESH_BAT0 = 0;
-        STOP_CHARGE_THRESH_BAT0 = 1;
-      };
-    };
-
-    auto-cpufreq = {
-      enable = false;
-      settings = {
-        battery = {
-          governor = "powersave";
-          turbo = "never";
-          energy_performance_preference = "power";
-          scaling_max_freq = 1600000;
-        };
-        charger = {
-          governor = "powersave";
-          turbo = "never";
-          energy_performance_preference = "power";
-          scaling_max_freq = 1600000;
-        };
-      };
-    };
-  };
+    # -- Choose one
+    ./power-profiles-daemon.nix
+    #./tlp.nix
+    #./auto-cpufreq.nix
+  ];
 }
