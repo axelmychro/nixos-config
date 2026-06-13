528fc7c3cab8aa8a08216ce6f72463883972be02
Axel <axelmychro@gmail.com>
2026-06-13 17:56:52 +0700

    Entries: Remove fedora

--- host/prts/modules/boot/entries.nix
+++ host/prts/modules/boot/entries.nix
@@ -2,7 +2,6 @@ _:
 let
   bootPartition = "800C-0267";
   gentooPartition = "e04ea3ec-7f23-4caa-bee1-1b0ebc3247b0";
-  fedoraPartition = "05aa19b1-9cfe-4736-9ef4-ad1046b62ff0";
   gentooKernelDir = "/boot/kernels-gentoo";
   zenKernelDir = "/boot/kernels-zen";
 in
@@ -23,12 +22,6 @@ in
         linux ${zenKernelDir}/vmlinuz root=UUID=${gentooPartition} rw quiet
         initrd ${zenKernelDir}/initramfs.img
       }
-    }
-    menuentry 'Fedora' --class fedora --class gnu-linux --class gnu --class os {
-      insmod part_gpt
-      insmod ext2
-      search --no-floppy --fs-uuid --set=root ${bootPartition}
-      chainloader /EFI/Fedora-boot/shimx64.efi
     }
   '';
 }
