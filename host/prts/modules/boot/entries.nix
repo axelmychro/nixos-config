18672ca48d3370bf44a9b51fb78575351d49c738
Axel <axelmychro@gmail.com>
2026-06-16 18:09:17 +0700

    Boot entries: Remove unused kernel

--- host/prts/modules/boot/entries.nix
+++ host/prts/modules/boot/entries.nix
@@ -1,26 +1,15 @@
 _:
 let
   gentooPartition = "e04ea3ec-7f23-4caa-bee1-1b0ebc3247b0";
-  gentooKernelDir = "/boot/kernels-gentoo";
-  zenKernelDir = "/boot/kernels-zen";
 in
 {
   boot.loader.grub.extraEntries = ''
-    submenu 'Gentoo' --class gentoo {
-      menuentry 'Gentoo Kernel' --class gentoo --class gnu-linux --class gnu --class os {
-        insmod part_gpt
-        insmod ext2
-        search --no-floppy --fs-uuid --set=root ${gentooPartition}
-        linux ${gentooKernelDir}/vmlinuz root=UUID=${gentooPartition}
-        initrd ${gentooKernelDir}/initramfs.img
-      }
-      menuentry 'Zen Kernel' --class gentoo --class gnu-linux --class gnu --class os {
-        insmod part_gpt
-        insmod ext2
-        search --no-floppy --fs-uuid --set=root ${gentooPartition}
-        linux ${zenKernelDir}/vmlinuz root=UUID=${gentooPartition}
-        initrd ${zenKernelDir}/initramfs.img
-      }
+    menuentry 'Gentoo' --class gentoo --class gnu-linux --class gnu --class os {
+      insmod part_gpt
+      insmod ext2
+      search --no-floppy --fs-uuid --set=root ${gentooPartition}
+      linux /boot/vmlinuz root=UUID=${gentooPartition}
+      initrd /boot/initramfs.img
     }
   '';
 }
