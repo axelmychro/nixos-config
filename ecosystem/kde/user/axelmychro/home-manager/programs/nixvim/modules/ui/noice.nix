531fbc01d1301c169d25c4f384bda8a158cd7924
Axel <axelmychro@gmail.com>
2026-06-20 11:10:20 +0700

    Nixvim modules(ui): Reinitialize noice

--- ecosystem/kde/user/axelmychro/home-manager/programs/nixvim/modules/ui/noice.nix
+++ ecosystem/kde/user/axelmychro/home-manager/programs/nixvim/modules/ui/noice.nix
@@ -1,5 +1,15 @@
 _: {
-  programs.nixvim.plugins.nui = {
+  programs.nixvim.plugins.noice = {
     enable = true;
+
+    settings = {
+      presets = {
+        bottom_search = true;
+        command_palette = true;
+        long_message_to_split = true;
+        inc_rename = false;
+        lsp_doc_border = false;
+      };
+    };
   };
 }
