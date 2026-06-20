a084c6f903eee6ea3c7b321af82c898e57f8e5e4
Axel <axelmychro@gmail.com>
2026-06-20 11:04:41 +0700

    Nixvim(lang): Rename shell-scripts

--- ecosystem/kde/user/axelmychro/home-manager/programs/nixvim/lang/index.nix
+++ ecosystem/kde/user/axelmychro/home-manager/programs/nixvim/lang/index.nix
@@ -1,7 +1,7 @@
 { config, ... }: {
   _module.args.grammars = config.programs.nixvim.plugins.treesitter.package.builtGrammars;
   imports = [
-    ./bash.nix
+    ./shell-scripts.nix
     ./c-many.nix
     ./fish.nix
     ./json.nix
--- ecosystem/kde/user/axelmychro/home-manager/programs/nixvim/lang/shell-scripts.nix
+++ ecosystem/kde/user/axelmychro/home-manager/programs/nixvim/lang/shell-scripts.nix
@@ -1,18 +1,21 @@
 { pkgs, grammars, ... }:
 {
+  home.packages = [
+    pkgs.shfmt
+  ];
+
   programs.nixvim = {
     plugins = {
       lsp.servers.bashls.enable = true;
+
       conform-nvim.settings.formatters_by_ft = {
         bash = [ "shfmt" ];
         sh = [ "shfmt" ];
       };
+
       treesitter.grammarPackages = with grammars; [
         bash
       ];
     };
-    extraPackages = with pkgs; [
-      shfmt
-    ];
   };
 }
