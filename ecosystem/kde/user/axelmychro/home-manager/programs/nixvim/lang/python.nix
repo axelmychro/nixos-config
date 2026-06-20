eb008f1bbeb4d73a7ede96ad0ebab466bb725366
Axel <axelmychro@gmail.com>
2026-06-20 11:07:07 +0700

    Nixvim(lang): Add pyright,ruff,black home packages

--- ecosystem/kde/user/axelmychro/home-manager/programs/nixvim/lang/python.nix
+++ ecosystem/kde/user/axelmychro/home-manager/programs/nixvim/lang/python.nix
@@ -1,20 +1,23 @@
 { pkgs, grammars, ... }:
 {
+  home.packages = with pkgs; [
+    pyright
+    ruff
+    black
+  ];
+
   programs.nixvim = {
     plugins = {
-      lsp.servers.pyright.enable = true;
-
-      conform-nvim.settings.formatters_by_ft = {
-        python = [ "black" ];
+      lsp.servers = {
+        pyright.enable = true;
+        ruff.enable = true;
       };
+
+      conform-nvim.settings.formatters_by_ft.python = [ "black" ];
 
       treesitter.grammarPackages = with grammars; [
         python
       ];
     };
-
-    extraPackages = with pkgs; [
-      black
-    ];
   };
 }
