4d4a1e804ba157acc48a6fc5d2470882da64e7c6
Axel <axelmychro@gmail.com>
2026-06-20 11:07:20 +0700

    Nixvim(lang): Add rustfmt home package

--- ecosystem/kde/user/axelmychro/home-manager/programs/nixvim/lang/rust.nix
+++ ecosystem/kde/user/axelmychro/home-manager/programs/nixvim/lang/rust.nix
@@ -1,19 +1,18 @@
 { pkgs, grammars, ... }:
 {
+  home.packages = [
+    pkgs.rustfmt
+  ];
+
   programs.nixvim = {
-    # rustaceanvim manages rust-analyzer internally
-    # do NOT enable plugins.lsp.servers.rust-analyzer alongside this
     plugins = {
       rustaceanvim.enable = true;
-      conform-nvim.settings.formatters_by_ft = {
-        rust = [ "rustfmt" ];
-      };
-      treesitter.grammarPackages = with grammars; [
-        rust
+
+      conform-nvim.settings.formatters_by_ft.rust = [ "rustfmt" ];
+
+      treesitter.grammarPackages = [
+        grammars.rust
       ];
     };
-    extraPackages = with pkgs; [
-      rustfmt
-    ];
   };
 }
