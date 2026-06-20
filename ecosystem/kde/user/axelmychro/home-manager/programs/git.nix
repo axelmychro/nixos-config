db9116b9a38b9f0182fd241d0012a5f5d82ca8fc
Axel <axelmychro@gmail.com>
2026-06-20 14:30:19 +0700

    Git: Remove old settings

--- ecosystem/kde/user/axelmychro/home-manager/programs/git.nix
+++ ecosystem/kde/user/axelmychro/home-manager/programs/git.nix
@@ -1,23 +1,10 @@
 _: {
   programs.git = {
-    enable = true;
-    settings = {
-      user = {
-        name = "Axel";
-        email = "axelmychro@gmail.com";
-      };
-      init.defaultBranch = "main";
-      pull.rebase = true;
-      alias = {
-        s = "status";
-        r = "remote -v";
-        l = "log --oneline";
-        co = "checkout";
-        f = "fetch";
-        sh = "stash";
-        pl = "pull";
-        ph = "push";
-      };
+    settings.user = {
+      name = "Axel";
+      email = "axelmychro@gmail.com";
     };
+
+    enable = true;
   };
 }
