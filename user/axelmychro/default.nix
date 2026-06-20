ef8e97a496658a3bdc01643bc539df0925add3e4
Axel <axelmychro@gmail.com>
2026-06-20 07:42:28 +0700

    User(axelmychro): Add ssh key

--- user/axelmychro/default.nix
+++ user/axelmychro/default.nix
@@ -17,6 +17,7 @@ in
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG6PBGNhpPnKvAjl2k0oZeY732xawJPcRM/G4yjc+vgR axelmychro@prts"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMG2joyLqVw/DLRWmlRm2yXCXAMTQYJKqrxzKazhERxv axelmychro@prts-web"
     ];
   };
 }
