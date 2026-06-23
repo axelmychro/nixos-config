86c8d31846c2363dddcd8abf1470ce9bd0b6a768
Axel <axelmychro@gmail.com>
2026-06-23 11:46:33 +0700

    Rename file to default

--- ecosystem/kde/user/axelmychro/default.nix
+++ ecosystem/kde/user/axelmychro/default.nix
@@ -14,7 +14,6 @@ in
   imports = [
     "${their.configuration}"
     ./packages/index.nix
-    ./home-manager/home.nix
   ];
 
   users.users.${them} = {
@@ -35,4 +34,27 @@ in
     ln -sfn -- "${assets}/${them}/face.png" "$_user_icon_dir/${them}"
     unset _user_icon_dir
   '';
+
+  home-manager = {
+    extraSpecialArgs = {
+      inherit
+        assets
+        plasma-manager
+        ;
+    };
+    sharedModules = [ plasma-manager.homeModules.plasma-manager ];
+
+    users.${user.name} = {
+      home = {
+        homeDirectory = "/home/${user.name}";
+        stateVersion = "26.05"; # HM is developed against nixos-unstable
+      };
+
+      imports = [
+        nixvim.homeModules.default
+        ./home-manager
+      ];
+    };
+  };
+
 }
---
+++ ecosystem/kde/user/axelmychro/home-manager/default.nix
@@ -0,0 +1,45 @@
+{
+  pkgs,
+  ...
+}:
+{
+  home = {
+    pointerCursor = {
+      enable = true;
+      package = pkgs.vanilla-dmz;
+      name = "Vanilla-DMZ";
+      size = 48;
+
+      x11 = {
+        enable = true;
+        defaultCursor = "left_ptr";
+      };
+      gtk.enable = true;
+    };
+  };
+
+  xdg.enable = true;
+
+  gtk = {
+    enable = false;
+    theme = {
+      name = "Nordic";
+      package = pkgs.nordic;
+    };
+    iconTheme = {
+      name = "Nordzy";
+      package = pkgs.nordzy-icon-theme;
+    };
+  };
+
+  qt = {
+    enable = true;
+    # platformTheme.name = "gtk";
+    # style.name = "Nordic";
+  };
+
+  imports = [
+    ./programs/index.nix
+    ./plasma-manager/index.nix
+  ];
+}
