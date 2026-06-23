1041dee4f7a5b8dc7f9bdda3f4660ece7a42ec66
Axel <axelmychro@gmail.com>
2026-06-23 15:53:31 +0700

    Fish: Delete unused function

--- ecosystem/kde/user/axelmychro/home-manager/programs/fish/functions/index.nix
+++ ecosystem/kde/user/axelmychro/home-manager/programs/fish/functions/index.nix
@@ -2,7 +2,6 @@ _: {
   programs.fish.shellInit = ''
     ${builtins.readFile ./log.fish}
     ${builtins.readFile ./conserve.fish}
-    ${builtins.readFile ./kya.fish}
     ${builtins.readFile ./dl.fish}
   '';
 }
