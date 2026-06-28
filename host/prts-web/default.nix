4a019585c645439d4b7585f47c4a444efac9d6a3
Axel <axelmychro@gmail.com>
2026-06-28 09:41:35 +0700

    Host(web): Allow https

--- host/prts-web/default.nix
+++ host/prts-web/default.nix
@@ -14,8 +14,17 @@
   services.nginx = {
     virtualHosts."192.168.122.185" = {
       root = "/var/www/dist";
+      addSSL = true;
+      enableACME = true;
     };
     enable = true;
   };
-  networking.firewall.allowedTCPPorts = [ 80 ];
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "axelmychro@gmail.com";
+  };
 }
