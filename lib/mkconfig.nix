66490caf3e974ee6d02c6e5d0c1c9bb49a6d70a3
Axel <axelmychro@gmail.com>
2026-07-01 00:37:06 +0700

    Lib(mkconfig): Initialize

---
+++ lib/mkconfig.nix
@@ -0,0 +1,61 @@
+{ inputs }:
+host:
+{
+  version ? "26.05",
+  assets ? ../assets,
+
+  ## Required
+  system,
+  users,
+  type ? "minimal",
+
+  ## Optional
+  gfx ? [ ],
+  extraArgs ? { },
+  extraModules ? [ ],
+}:
+
+let
+  userConfigurations = map (u: ../user/${u}) users;
+  gfxModules = map (m: ../modules/graphics/${m}.nix) gfx;
+in
+
+inputs.nixpkgs.lib.nixosSystem {
+  specialArgs = {
+    inherit
+      system
+      version
+      type
+      assets
+      ;
+  }
+  // extraArgs;
+
+  modules = [
+    { system.nixos.label = "${type}-${version}"; }
+    ../host/${host}
+
+    ../modules/networking
+    ./nixosconfiguser.nix
+  ]
+  ++ userConfigurations
+  ++ (
+    if type == "minimal" then
+      [ ]
+    else if type == "desktop" then
+      [
+        ../modules/login
+        ../modules/home-manager
+        ../modules/audio
+        ../modules/bluetooth
+        ../modules/power
+        ../modules/graphics
+      ]
+    else if type == "server" then
+      [ ]
+    else
+      [ ]
+  )
+  ++ gfxModules
+  ++ extraModules;
+}
