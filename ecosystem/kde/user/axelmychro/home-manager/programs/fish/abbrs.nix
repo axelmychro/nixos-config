e8a6cdd7b239ae3c1addce21b8f1e34d68a3bf40
Axel <axelmychro@gmail.com>
2026-07-01 00:36:04 +0700

    Move abbr to main as alias

--- ecosystem/kde/user/axelmychro/home-manager/programs/fish/abbrs.nix
+++ ecosystem/kde/user/axelmychro/home-manager/programs/fish/abbrs.nix
@@ -1,6 +1,5 @@
 _: {
   programs.fish.shellAbbrs = {
-    nc = "cd ~/nixos-config";
     fetch = "fastfetch";
   };
 }
--- host/prts/default.nix
+++ host/prts/default.nix
@@ -13,4 +13,6 @@ _: {
   };
   services.fwupd.enable = true; # firmware update daemon
   zramSwap.enable = true; # 50% by default
+
+  environment.shellAliases.nc = "cd ~/nixos-config";
 }
