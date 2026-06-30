1c4f8e7f7e94bde191b5d19519eb44db0714e8d2
Axel <axelmychro@gmail.com>
2026-06-30 21:53:22 +0700

    Remove duplicate abbr and alias

--- ecosystem/kde/user/axelmychro/home-manager/programs/fish/abbrs.nix
+++ ecosystem/kde/user/axelmychro/home-manager/programs/fish/abbrs.nix
@@ -1,18 +1,6 @@
 _: {
   programs.fish.shellAbbrs = {
-    md = "mkdir -p";
-    c = "cd";
-    s = "sudo";
-    e = "$EDITOR";
-    sue = "sudoedit";
-    g = "git";
-    nxsh = "nix-shell -p";
-
     nc = "cd ~/nixos-config";
-
-    mt = "mount";
-    umt = "umount";
-
-    ff = "fastfetch";
+    fetch = "fastfetch";
   };
 }
--- ecosystem/kde/user/axelmychro/home-manager/programs/fish/aliases.nix
+++ ecosystem/kde/user/axelmychro/home-manager/programs/fish/aliases.nix
@@ -1,10 +1,7 @@
 _: {
   programs.fish.shellAliases = {
-    x = "sync; clear; exec $SHELL";
     l = "eza --almost-all --long --group-directories-first --git";
     lm = "l --sort=modified";
-    ".." = "cd ..";
-    "..." = "cd ../..";
     t = "tmux attach 2>/dev/null; or tmux";
   };
 }
