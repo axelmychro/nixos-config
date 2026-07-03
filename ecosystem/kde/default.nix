002743df656f9b8c402bdcebbefd1267c0f5b856
Axel <axelmychro@gmail.com>
2026-07-03 14:34:30 +0700

    Eco(kde): Exclude packages

--- ecosystem/kde/default.nix
+++ ecosystem/kde/default.nix
@@ -1,8 +1,16 @@
 { pkgs, ... }: {
   services.desktopManager.plasma6.enable = true;
-  environment.systemPackages = with pkgs; [
-    haruna
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      haruna
+      ncmpcpp
+    ];
+
+    plasma6.excludePackages = with pkgs.kdePackages; [
+      plasma-browser-integration
+      elisa
+    ];
+  };
 
   imports = [ ./user/index.nix ];
 }
