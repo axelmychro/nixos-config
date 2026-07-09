e2e413757ac1d20dc91d0854260938cb0ed99c2c
Axel <axelmychro@gmail.com>
2026-07-09 08:38:02 +0700

    Eco(niri): Enable essential system package

--- ecosystem/niri/default.nix
+++ ecosystem/niri/default.nix
@@ -10,9 +10,11 @@
   services.gnome.gnome-keyring.enable = true;
 
   environment.systemPackages = with pkgs; [
-    noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
     xwayland-satellite
-    cliphist
+    firefox
+    noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+    alacritty
+    tmux
   ];
 
   xdg.portal.config.niri = {
