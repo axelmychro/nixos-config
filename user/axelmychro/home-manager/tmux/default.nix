8fcf3f42d91e6392fa2ad4c50bffe98806f401a0
Axel <axelmychro@gmail.com>
2026-08-02 10:50:13 +0700

    User(axelmychro): Swap tmux status colours

--- user/axelmychro/home-manager/tmux/default.nix
+++ user/axelmychro/home-manager/tmux/default.nix
@@ -1,13 +1,12 @@
 _: {
   programs.tmux = {
     enable = true;
-    terminal = "tmux-256color";
     prefix = "C-z";
     keyMode = "vi";
     focusEvents = true;
 
     extraConfig = ''
-      set-option -g status-style bg=#c6a0f6,fg=#24273a
+      set-option -g status-style bg=#181926,fg=#cad3f5
     '';
   };
 }
