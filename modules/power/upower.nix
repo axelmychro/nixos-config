fc3eac39d7560b2563ef54f157f403645fe8431e
Axel <axelmychro@gmail.com>
2026-08-06 16:54:26 +0700

    Mod(power): Update upower config

--- modules/power/upower.nix
+++ modules/power/upower.nix
@@ -1,28 +1,8 @@
 _: {
-  services = {
-    upower = {
-      enable = true;
-      percentageLow = 50;
-      percentageCritical = 20;
-      ignoreLid = true;
-    };
-
-    auto-cpufreq = {
-      enable = false;
-      settings = {
-        battery = {
-          governor = "powersave";
-          turbo = "never";
-          energy_performance_preference = "power";
-          scaling_max_freq = 1600000;
-        };
-        charger = {
-          governor = "powersave";
-          turbo = "never";
-          energy_performance_preference = "power";
-          scaling_max_freq = 1600000;
-        };
-      };
-    };
+  services.upower = {
+    enable = true;
+    percentageLow = 50;
+    percentageCritical = 20;
+    ignoreLid = true;
   };
 }
