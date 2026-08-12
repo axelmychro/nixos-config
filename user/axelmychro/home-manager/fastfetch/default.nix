1a15159d874fd6b0cb39ff32f0321ffb818678db
Axel <axelmychro@gmail.com>
2026-08-13 00:21:24 +0700

    User(axelmychro): Enable fastfetch config alt

--- user/axelmychro/home-manager/fastfetch/default.nix
+++ user/axelmychro/home-manager/fastfetch/default.nix
@@ -3,5 +3,8 @@ _: {
   xdg.configFile = {
     "fastfetch/config.jsonc".source = ./config.jsonc;
     "fastfetch/nix.txt".source = ./src/nix.txt;
+
+    "fastfetch/zconf.json".source = ./zconf.json;
+    "fastfetch/closure.png".source = ./src/closure.png;
   };
 }
