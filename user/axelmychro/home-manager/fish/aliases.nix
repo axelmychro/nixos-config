a612c068e068c8b8922dc8cd8f65959c2bbae84e
Axel <axelmychro@gmail.com>
2026-08-13 00:21:39 +0700

    User(axelmychro): Enable fish alias zfetch

--- user/axelmychro/home-manager/fish/aliases.nix
+++ user/axelmychro/home-manager/fish/aliases.nix
@@ -3,5 +3,6 @@ _: {
     l = "eza --almost-all --long --group-directories-first --git";
     lm = "l --sort=modified";
     t = "tmux attach 2>/dev/null; or tmux";
+    zfetch = "fastfetch --config ~/.config/fastfetch/zconf.json";
   };
 }
