719579ebc9058e55ae9bb64674e9b36c5e385f01
Axel <axelmychro@gmail.com>
2026-08-15 11:39:31 +0700

    Rename zfetch to cfetch
    Closure!

--- user/axelmychro/home-manager/fastfetch/default.nix
+++ user/axelmychro/home-manager/fastfetch/default.nix
@@ -4,7 +4,7 @@ _: {
     "fastfetch/config.jsonc".source = ./config.jsonc;
     "fastfetch/nix.txt".source = ./src/nix.txt;
 
-    "fastfetch/zconf.json".source = ./zconf.json;
+    "fastfetch/cconf.json".source = ./cconf.json;
     "fastfetch/closure.png".source = ./src/closure.png;
   };
 }
