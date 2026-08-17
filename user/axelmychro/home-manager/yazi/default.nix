6656ab19b7b86964241a59d74911e5ab966ddf33
Axel <axelmychro@gmail.com>
2026-08-17 21:40:35 +0700

    Yazi: Swap to local theme

--- user/axelmychro/home-manager/yazi/default.nix
+++ user/axelmychro/home-manager/yazi/default.nix
@@ -1,30 +1,10 @@
-{ pkgs, ... }:
-let
-  rose_pine_yazi_repo = pkgs.fetchFromGitHub {
-    owner = "rose-pine";
-    repo = "yazi";
-    rev = "main";
-    hash = "sha256-9e3dXViWl1rK9BPrGAFfs9ZL/tsG6Njz6ksuU6AIrFY=";
-  };
-in
-{
+_: {
   programs.yazi = {
     flavors = {
-      rose-pine = pkgs.runCommand "rose-pine.yazi" { } ''
-        cp -r ${rose_pine_yazi_repo}/flavors/rose-pine.yazi $out
-      '';
-      rose-pine-dawn = pkgs.runCommand "rose-pine-dawn.yazi" { } ''
-        cp -r ${rose_pine_yazi_repo}/flavors/rose-pine-dawn.yazi $out
-      '';
+      rose-pine = ./flavors/rose-pine.yazi;
+      rose-pine-dawn = ./flavors/rose-pine-dawn.yazi;
     };
     enable = true;
   };
-
-  xdg.configFile."yazi/theme.toml".text = ''
-    [flavor]
-    light = "rose-pine-dawn"
-    dark = "rose-pine"
-
-    ${builtins.readFile "${rose_pine_yazi_repo}/themes/rose-pine-dawn.toml"}
-  '';
+  xdg.configFile."yazi/theme.toml".source = ./theme.toml;
 }
