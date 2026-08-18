5e478cb6ec31bbb6d7c35964d343dd3fd182504e
Axel <axelmychro@gmail.com>
2026-08-18 07:04:27 +0700

    Cava: Use local config

--- user/axelmychro/home-manager/cava/default.nix
+++ user/axelmychro/home-manager/cava/default.nix
@@ -1,16 +1,16 @@
-{ pkgs, ... }:
-let
-  rose_pine_cava_repo = pkgs.fetchFromGitHub {
-    owner = "rose-pine";
-    repo = "cava";
-    rev = "main";
-    hash = "sha256-i0Z5ZFygo+AjCo9NlwBhA41srwsDgAc7GPXPHCWngPc=";
-  };
-in
-{
+_: {
   programs.cava.enable = true;
   xdg.configFile."cava/config".text = ''
     [color]
-    ${builtins.readFile "${rose_pine_cava_repo}/dawn"}
+    background = '#faf4ed'
+    gradient = 1
+    gradient_count = 6
+    gradient_color_1 = '#286983'
+    gradient_color_2 = '#56949f'
+    gradient_color_3 = '#907aa9'
+    gradient_color_4 = '#b4637a'
+    gradient_color_5 = '#d7827e'
+    gradient_color_6 = '#ea9d34'
+
   '';
 }
