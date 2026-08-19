112a168bd22cb898c745984d213e7a5657971ba9
Axel <axelmychro@gmail.com>
2026-08-19 12:08:44 +0700

    Eco: Update common desktop config

--- ecosystem/common.nix
+++ ecosystem/common.nix
@@ -1,63 +1,101 @@
-{ pkgs, prismlauncher, ... }:
-# Shared packages for desktops
 {
-  programs = {
-    nix-ld.enable = true;
-    steam = {
-      package = pkgs.millennium-steam;
-      enable = true;
-    };
-    gamemode.enable = true;
+  lib,
+  pkgs,
+  prismlauncher,
+  ...
+}:
+# Shared packages for desktops
+let
+  binaryCompatibility = {
+    environment.systemPackages = with pkgs; [
+      binutils
+      steam-run
+    ];
+    programs.nix-ld.enable = true;
   };
-  environment = {
-    systemPackages = with pkgs; [
-      wl-clipboard # wl-copy
-
-      # Terminalism
-      kitty
+  myTerminalism = {
+    environment.systemPackages = with pkgs; [
+      kitty # Emulator
       yazi
       fastfetch
       btop
       gdu
+      tree
       wiremix
-
-      # Browser
-      firefox
-      # Documents
-      libreoffice
-
-      # Multimedia
+      wl-clipboard # wl-copy
+    ];
+  };
+  gstreamerFull = {
+    environment = {
+      systemPackages = with pkgs; [
+        # NOTE: Some programs like cosmic-player requires gstreamer.
+        gst_all_1.gstreamer
+        gst_all_1.gst-plugins-base
+        gst_all_1.gst-plugins-good
+        gst_all_1.gst-plugins-bad
+        gst_all_1.gst-plugins-ugly
+        gst_all_1.gst-libav
+        gst_all_1.gst-vaapi
+      ];
+      variables.GST_PLUGIN_PATH = "/run/current-system/sw/lib/gstreamer-1.0/";
+    };
+  };
+  myMultimedia = {
+    environment.systemPackages = with pkgs; [
+      ffmpeg
+      imagemagick
       gimp
       krita
       kdePackages.kdenlive
       blender
-      zoom-us
+      unityhub
       easyeffects
-
-      # NOTE: Some programs like cosmic-player requires gstreamer
-      gst_all_1.gstreamer
-      gst_all_1.gst-plugins-base
-      gst_all_1.gst-plugins-good
-      gst_all_1.gst-plugins-bad
-      gst_all_1.gst-plugins-ugly
-      gst_all_1.gst-libav
-      gst_all_1.gst-vaapi
-
-      # Binary compatibilities
-      binutils
-      #nix-ld
-      steam-run
-
-      # Games
+    ];
+    programs = {
+      obs-studio = {
+        package = pkgs.obs-studio.override {
+          cudaSupport = true;
+        };
+        enable = true;
+        enableVirtualCamera = true;
+      };
+    };
+  };
+  myGames = {
+    environment.systemPackages = with pkgs; [
       wineWow64Packages.waylandFull
       winetricks
-      #gamemode
       protonplus
+      #gamemode
       #steam
       heroic
       #osu-lazer-bin
       prismlauncher.packages.${pkgs.system}.prismlauncher
     ];
-    variables.GST_PLUGIN_PATH = "/run/current-system/sw/lib/gstreamer-1.0/";
+    programs = {
+      gamemode.enable = true;
+      steam = {
+        enable = true;
+        package = pkgs.millennium-steam;
+      };
+    };
   };
-}
+in
+lib.mkMerge [
+  binaryCompatibility
+  myTerminalism
+  {
+    environment.systemPackages = with pkgs; [
+      # Browser
+      firefox
+
+      # Univ & office
+      libreoffice
+      zoom-us
+    ];
+
+  }
+  gstreamerFull
+  myMultimedia
+  myGames
+]
