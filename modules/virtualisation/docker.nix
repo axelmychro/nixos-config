ac5bd78709e9a8e9325de48848f69c32dba6f914
Axel <axelmychro@gmail.com>
2026-08-28 21:09:22 +0700

    Mod (virt) docker: Add note

--- modules/virtualisation/docker.nix
+++ modules/virtualisation/docker.nix
@@ -1,3 +1,5 @@
 _: {
   virtualisation.docker.enable = true;
+  # NOTE: If you use the btrfs filesystem, you might need to set the storageDriver option:
+  #virtualisation.docker.storageDriver = "btrfs";
 }
