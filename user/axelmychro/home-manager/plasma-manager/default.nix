f45e55203404e6123a70498d95429b92fafcd1b8
Axel <axelmychro@gmail.com>
2026-08-30 21:23:53 +0700

    Plasma-manager: Update config

--- user/axelmychro/home-manager/plasma-manager/default.nix
+++ user/axelmychro/home-manager/plasma-manager/default.nix
@@ -1,19 +1,52 @@
-{ wallpaper-file, ... }: {
+{ lib, wallpaper-file, ... }:
+{
   programs.plasma = {
     enable = true;
     overrideConfig = true;
 
     workspace = {
+      # light = org.kde.breeze.desktop
+      # dark = org.kde.breezedark.desktop
+      lookAndFeel = "org.kde.breeze.desktop";
+      colorScheme = "FOOBAR";
       wallpaper = wallpaper-file;
     };
+    kscreenlocker.appearance = {
+      wallpaper = wallpaper-file;
+      alwaysShowClock = true;
+      showMediaControls = true;
+    };
+    fonts =
+      let
+        family = "GoMono Nerd Font";
+        pointSize = 12;
+      in
+      lib.genAttrs
+        [
+          "general"
+          "fixedWidth"
+          "small"
+          "toolbar"
+          "menu"
+          "windowTitle"
+        ]
+        (_: {
+          inherit family pointSize;
+        });
+
+    input.mice = [
+      {
+        vendorId = "046d";
+        productId = "c077";
+        name = "Logitech USB Optical Mouse";
+        enable = true;
+        acceleration = 0;
+        accelerationProfile = "none";
+        leftHanded = false;
+        middleButtonEmulation = false;
+        naturalScroll = false;
+        scrollSpeed = 1;
+      }
+    ];
   };
-  #imports = [
-  #  ./desktop.nix
-  #  ./devices.nix
-  #  ./panels.nix
-  #  ./power.nix
-  #  ./session.nix
-  #  ./shortcuts.nix
-  #  ./window.nix
-  #];
 }
