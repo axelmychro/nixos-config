a1095845502e9857deba093fc021f62dbf501ba8
Axel <axelmychro@gmail.com>
2026-08-30 19:57:16 +0700

    Konsole: Move out from Eco (kde)

--- user/axelmychro/home-manager/konsole/default.nix
+++ user/axelmychro/home-manager/konsole/default.nix
@@ -1,7 +1,5 @@
 { pkgs, ... }:
 {
-  home.packages = [
-    pkgs.kdePackages.konsole
-  ];
+  home.packages = [ pkgs.kdePackages.konsole ];
   xdg.dataFile."konsole/catppuccin-macchiato.colorscheme".source = ./catppuccin-macchiato.colorscheme;
 }
