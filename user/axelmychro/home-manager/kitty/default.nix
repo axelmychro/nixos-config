a0e765e8ef917d5506861ff6139221505816ca3d
Axel <axelmychro@gmail.com>
2026-08-30 22:49:30 +0700

    Kitty: Add Rose Pine theme

--- user/axelmychro/home-manager/kitty/default.nix
+++ user/axelmychro/home-manager/kitty/default.nix
@@ -1,4 +1,15 @@
-_: {
+{ theme, ... }:
+let
+  kitty_conf = if theme == "rose-pine" then ./kitty-rose_pine.conf else ./kitty-rose_pine_dawn.conf;
+in
+{
   programs.kitty.enable = true;
-  xdg.configFile."kitty/kitty.conf".source = ./kitty.conf;
+  xdg.configFile."kitty/kitty.conf".text = ''
+    hide_window_decorations titlebar-only
+    font_family             GoMono Nerd Font
+    font_size               16.0
+    background_opacity      0.9
+
+    ${builtins.readFile kitty_conf}
+  '';
 }
