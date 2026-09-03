6b6fc366e777359669ab5e1102805dcbd893fcca
Axel <axelmychro@gmail.com>
2026-09-03 18:52:05 +0700

    Nixvim: Merge config files

--- user/axelmychro/home-manager/nixvim/default.nix
+++ user/axelmychro/home-manager/nixvim/default.nix
@@ -1,8 +1,38 @@
-_: {
-  programs.nixvim.enable = true;
+{ theme, ... }:
+let
+  colorschemes = {
+    rose-pine = {
+      enable = true;
+      settings.variant = if theme == "rose-pine" then "main" else "dawn";
+    };
+  };
+  colorscheme = if theme == "rose-pine" || theme == "rose-pine-dawn" then "rose-pine" else "";
+in
+{
+  programs.nixvim = {
+    inherit colorschemes colorscheme;
+
+    globals = {
+      mapleader = " ";
+      maplocalleader = "\\";
+
+      autoformat = true;
+      snacks_animate = true;
+      #lazyvim_picker = "auto";
+      #lazyvim_cmp = "auto";
+
+      #deprecation_warnings = false;
+      #trouble_lualine = true;
+    };
+
+    clipboard = {
+      register = "unnamedplus";
+      providers.wl-copy.enable = true;
+    };
+    enable = true;
+  };
 
   imports = [
-    ./general.nix
     ./keymaps/index.nix
     ./plugins/index.nix
     ./lang/index.nix
