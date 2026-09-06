471c53c89e2e6f24d2e71370ddef1cbd823d8364
Axel <axelmychro@gmail.com>
2026-09-06 16:41:24 +0700

    Common (desktop) development: Enable postgresql

--- common/desktop/development.nix
+++ common/desktop/development.nix
@@ -4,9 +4,27 @@
   config,
   ...
 }:
+let
+  ai_packages = lib.optional config.common.development.ai.enable (
+    with pkgs;
+    [
+      aichat
+      upscayl
+      video2x
+    ]
+  );
+in
 {
   config = lib.mkIf config.common.development.enable {
     services = {
+      postgresql = {
+        enable = true;
+        ensureDatabases = [ "mydatabase" ];
+        authentication = pkgs.lib.mkOverride 10 ''
+          #type database  DBuser  auth-method
+          local all       all     trust
+        '';
+      };
       ollama = {
         enable = config.common.development.ai.enable;
         package = pkgs.ollama-cuda;
@@ -18,15 +36,13 @@
       };
     };
     programs.tmux.enable = true;
-    environment.systemPackages = [
-      pkgs.zed-editor
-      pkgs.video2x
-    ]
-    ++ lib.optional config.common.development.ai.enable [
-      pkgs.aichat
-      pkgs.upscayl
-      pkgs.video2x
-    ];
+    environment.systemPackages =
+      with pkgs;
+      [
+        zed-editor
+        video2x
+      ]
+      ++ ai_packages;
   };
   options.common.development = {
     enable = lib.mkEnableOption "Common desktop development options";
