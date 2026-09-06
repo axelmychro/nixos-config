6ba536c5e1b815b4a5a57186bf520b3d8ecf2ebf
Axel <axelmychro@gmail.com>
2026-09-06 18:54:51 +0700

    User (axelmchro) fish: Remove unused functions

--- user/axelmychro/home-manager/fish/functions/dl-video.fish
+++
@@ -1,6 +0,0 @@
-function dl-video
-    set -- url "$argv[1]"
-    test -z "$url"; and log -e 'No URL provided'; and return
-
-    nix-shell -p yt-dlp --run "yt-dlp -f 'bestvideo' '$url'"
-end
--- user/axelmychro/home-manager/fish/functions/index.nix
+++ user/axelmychro/home-manager/fish/functions/index.nix
@@ -2,7 +2,5 @@ _: {
   programs.fish.shellInit = ''
     ${builtins.readFile ./log.fish}
     ${builtins.readFile ./conserve-battery.fish}
-    ${builtins.readFile ./install-osu-bin.fish}
-    ${builtins.readFile ./dl-video.fish}
   '';
 }
