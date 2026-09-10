8b00e048dde6e215b6f246aaa367091cb55bb1cd
Axel <axelmychro@gmail.com>
2026-09-10 07:49:29 +0700

    Common (desktop) development: Enable pgadmin

--- common/desktop/development.nix
+++ common/desktop/development.nix
@@ -13,18 +13,33 @@ let
       video2x
     ]
   );
+  host_name = config.networking.hostName;
 in
 {
   config = lib.mkIf config.common.development.enable {
     services = {
       postgresql = {
         enable = true;
-        ensureDatabases = [ "mydatabase" ];
+        ensureDatabases = [ host_name ];
+        enableTCPIP = true;
+        # port = 5432;
         authentication = pkgs.lib.mkOverride 10 ''
-          #type database  DBuser  auth-method
-          local all       all     trust
+          #type database DBuser origin-address auth-method
+          local all      all     trust
+          # ... other auth rules ...
+
+          # ipv4
+          host  all      all     127.0.0.1/32   trust
+          # ipv6
+          host  all      all     ::1/128        trust
+        '';
+        initialScript = pkgs.writeText "initialScript" ''
+          CREATE ROLE ${host_name} WITH LOGIN PASSWORD '${host_name}' CREATEDB;
+          CREATE DATABASE ${host_name};
+          GRANT ALL PRIVILEGES ON DATABASE ${host_name} TO ${host_name};
         '';
       };
+      pgadmin.enable = true;
       ollama = {
         enable = config.common.development.ai.enable;
         package = pkgs.ollama-cuda;
