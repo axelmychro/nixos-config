c6c84ae4579d9e57caf8a9c54e45a5f474a99dd2
Axel <axelmychro@gmail.com>
2026-09-12 17:27:19 +0700

    User (axelmychro) nixcord: Enable theming

--- user/axelmychro/home-manager/nixcord/default.nix
+++ user/axelmychro/home-manager/nixcord/default.nix
@@ -1,11 +1,18 @@
+{ inputs, theme, ... }:
 {
+  imports = [ inputs.nixcord.homeModules.default ];
   programs.nixcord = {
-    config.frameless = true;
-    discord = {
-      equicord.enable = true;
-      enable = false;
+    quickCss =
+      if theme == "rose-pine" then
+        builtins.readFile ./rose-pine.css
+      else
+        builtins.readFile ./rose-pine-dawn.css;
+    config = {
+      frameless = true;
+      useQuickCss = true;
     };
-    equibop.enable = true;
+    discord.enable = false;
     enable = true;
+    equibop.enable = true;
   };
 }
