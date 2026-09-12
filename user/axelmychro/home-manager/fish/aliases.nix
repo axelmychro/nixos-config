d8708a06207b12b448afe0172682c65759b78c94
Axel <axelmychro@gmail.com>
2026-09-12 17:28:49 +0700

    User (axelmychro) fish: Enable eza aliases

--- user/axelmychro/home-manager/fish/aliases.nix
+++ user/axelmychro/home-manager/fish/aliases.nix
@@ -1,7 +1,9 @@
-_: {
+{
   programs.fish.shellAliases = {
     l = "eza --almost-all --long --group-directories-first --git";
-    lm = "l --sort=modified";
+    ll = "eza --long";
+    lm = "eza --almost-all --long --sort=modified";
+    lt = "eza --tree";
     t = "tmux attach 2>/dev/null; or tmux";
     cfetch = "fastfetch --config ~/.config/fastfetch/cconf.json";
   };
