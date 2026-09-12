d58d658c37ab66687d6e92ba32056e3321447ce6
Axel <axelmychro@gmail.com>
2026-09-12 14:07:58 +0700

    Common (desktop): Colour login screen spinner white

--- common/desktop/display-manager.nix
+++ common/desktop/display-manager.nix
@@ -6,6 +6,9 @@
 let
   sddm_bg_file_name = "amiya.mp4";
   font_family_name = "GoMono Nerd Font";
+  white = ''
+    "#FFFFFF"
+  '';
 in
 {
   imports = [
@@ -20,26 +23,33 @@ in
     backgrounds.${sddm_bg_file_name} = "${assets}/${sddm_bg_file_name}";
     settings = {
       "LockScreen".background = sddm_bg_file_name;
+      "LockScreen.Date".color = white;
+      "LockScreen.Clock".color = white;
+
       "LoginScreen".background = sddm_bg_file_name;
-      "LockScreen.Clock".color = "#FFFFFF";
-      "LockScreen.Date".color = "#FFFFFF";
       "LoginScreen.Clock".font-family = font_family_name;
       "LoginScreen.Date".font-family = font_family_name;
-      "LoginScreen.Message".font-family = font_family_name;
+      "LoginScreen.Message" = {
+        font-family = font_family_name;
+        color = white;
+      };
       "LoginScreen.LoginArea.Avatar".shape = "square";
       "LoginScreen.LoginArea.Username" = {
         font-family = font_family_name;
-        color = "#FFFFFF";
+        color = white;
       };
       "LoginScreen.LoginArea.PasswordInput".font-family = font_family_name;
       "LoginScreen.LoginArea.LoginButton".font-family = font_family_name;
-      "LoginScreen.LoginArea.Spinner".font-family = font_family_name;
+      "LoginScreen.LoginArea.Spinner" = {
+        font-family = font_family_name;
+        color = white;
+      };
       "LoginScreen.LoginArea.WarningMessage" = {
         font-family = font_family_name;
         font-weight = 700;
-        normal-color = "#FFFFFF";
-        warning-color = "#FFFFFF";
-        error-color = "#FFFFFF";
+        normal-color = white;
+        warning-color = white;
+        error-color = white;
       };
       "LoginScreen.LoginArea.Buttons".font-family = font_family_name;
       "LoginScreen.LoginArea.Popups".font-family = font_family_name;
