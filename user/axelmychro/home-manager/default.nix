5a3a3348549afb0bd650513d1ba1efa9a940d05d
Axel <axelmychro@gmail.com>
2026-09-12 17:28:22 +0700

    User (axelmychro) editorconfig: Initialise

--- user/axelmychro/home-manager/default.nix
+++ user/axelmychro/home-manager/default.nix
@@ -9,6 +9,20 @@
   ...
 }:
 {
+  users.users.${user} = {
+    shell = pkgs.fish;
+    extraGroups = [
+      "video"
+      "render"
+      "docker"
+      "libvirtd"
+    ];
+    packages = with pkgs; [
+      eza
+      ripgrep
+      fd
+    ];
+  };
   home-manager = {
     extraSpecialArgs = {
       inherit
@@ -28,15 +42,27 @@
         pointerCursor = {
           package = pkgs.rose-pine-cursor;
           name = "BreezeX-RosePineDawn-Linux";
-          size = 48;
+          size = 64;
           enable = true;
           x11.enable = true;
           gtk.enable = true;
         };
       };
       xdg.enable = true;
-      qt.enable = false;
-      gtk.enable = false;
+
+      editorconfig = {
+        enable = true;
+        settings = {
+          "*" = {
+            charset = "utf-8";
+            end_of_line = "lf";
+            trim_trailing_whitespace = true;
+            insert_final_newline = true;
+            max_line_width = 80;
+          };
+        };
+      };
+
       imports = [
         ./bash
         ./cava
@@ -45,13 +71,12 @@
         ./fastfetch
         ./fish
         ./git
+        ./glow
         ./hyfetch
         ./kitty
         ./konsole
         ./lazygit
-        inputs.nixcord.homeModules.default
         ./nixcord
-        inputs.nixvim.homeModules.default
         ./nixvim
         ./oh-my-posh
         ./plasma-manager
@@ -64,19 +89,5 @@
   programs = {
     fish.enable = true;
     gnupg.agent.enable = true;
-  };
-  users.users.${user} = {
-    extraGroups = [
-      "video"
-      "render"
-      "docker"
-      "libvirtd"
-    ];
-    packages = with pkgs; [
-      eza
-      ripgrep
-      fd
-    ];
-    shell = pkgs.fish;
   };
 }
