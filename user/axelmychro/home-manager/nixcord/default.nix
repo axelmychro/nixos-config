9949354febf4f6f02ef0852ece067ba670e01a96
Axel <axelmychro@gmail.com>
2026-09-21 18:30:44 +0700

    User (axelmychro) nixcord: Use stylix

--- user/axelmychro/home-manager/nixcord/default.nix
+++ user/axelmychro/home-manager/nixcord/default.nix
@@ -1,18 +1,10 @@
-{ inputs, theme, ... }:
+{ inputs, ... }:
 {
   imports = [ inputs.nixcord.homeModules.default ];
   programs.nixcord = {
-    quickCss =
-      if theme == "rose-pine" then
-        builtins.readFile ./rose-pine.css
-      else
-        builtins.readFile ./rose-pine-dawn.css;
-    config = {
-      frameless = true;
-      useQuickCss = true;
-    };
-    discord.enable = false;
     enable = true;
+    config.frameless = true;
+    discord.enable = false;
     equibop.enable = true;
   };
 }
