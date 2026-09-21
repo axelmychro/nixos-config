17aa9120367c029bf11b3cb3359d2300e56c0488
Axel <axelmychro@gmail.com>
2026-09-21 10:57:04 +0700

    User (axelmychro) hyprland: Bind shift+print to fullscreen screenshot

--- user/axelmychro/home-manager/hypr/hyprland_noctalia.lua
+++ user/axelmychro/home-manager/hypr/hyprland_noctalia.lua
@@ -9,6 +9,7 @@ hl.bind(MAIN_MOD .. " + C", hl.dsp.exec_cmd(ipc .. "panel-toggle control-center"
 hl.bind(MAIN_MOD .. " + comma", hl.dsp.exec_cmd(ipc .. "settings-toggle"))
 hl.bind("ALT + Tab", hl.dsp.exec_cmd(ipc .. "window-switcher"))
 hl.bind("Print", hl.dsp.exec_cmd(ipc .. "screenshot-region"))
+hl.bind("SHIFT + Print", hl.dsp.exec_cmd(ipc .. "screenshot-fullscreen"))
 hl.bind(MAIN_MOD .. " + Escape", hl.dsp.exec_cmd(ipc .. "session lock"))
 
 -- Media keys
@@ -21,7 +22,7 @@ hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd(ipc .. "brightness-down"))
 
 -- Noctalia Settings
 hl.window_rule({
-	match = { class = "dev.noctalia.Noctalia" },
-	float = true,
-	size = { 1080, 920 },
+    match = { class = "dev.noctalia.Noctalia" },
+    float = true,
+    size = { 1080, 920 },
 })
